597dda349abc29a51a649573655601343564d3f7
aw3s0m3ss <aw3s0m3ss@gmail.com>
2024-10-19 19:47:10 +0300

    added cors to server

--- src/index.ts
+++ src/index.ts
@@ -4,17 +4,35 @@ import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
 import compression from 'compression';
 import cors from 'cors';
-
 import router from './router';
 import database from './db/connector'; // Adjust path as needed
 import dotenv from 'dotenv';
+
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT as string;
 
 // Middleware setup
-app.use(cors({ credentials: true }));
+const allowedOrigins = [
+    'http://localhost:5173',  // Local development
+    'https://react-demo-38cb3.web.app', // Production frontend
+];
+
+const corsOptions = {
+    origin: (origin: string | undefined, callback: (error: Error | null, allow?: boolean) => void) => {
+        // Allow requests with no origin (like mobile apps or Postman)
+        if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    credentials: true, // Allow credentials such as cookies
+};
+
+// Apply CORS middleware
+app.use(cors(corsOptions));
 app.use(compression());
 app.use(cookieParser());
 app.use(bodyParser.json());
