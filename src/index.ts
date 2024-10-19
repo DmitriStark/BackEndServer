b80e0e4ba66090e4e97847a9e82fb24e9396aa97
aw3s0m3ss <aw3s0m3ss@gmail.com>
2024-10-19 18:59:59 +0300

    completede users and authentication methods

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,36 @@
-import express from "express";
-import http from "http";
-import bodyParser from "body-parser";
-import cookieParser from "cookie-parser";
-import compression from "compression";
-import cors from "cors";
+import express from 'express';
+import http from 'http';
+import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
+import compression from 'compression';
+import cors from 'cors';
+
+import router from './router';
+import mongoose from 'mongoose';
 require('dotenv').config();
 
 const app = express();
-app.use(
-  cors({
-    credentials: true,
-  })
-);
+app.use(cors({
+    credentials:true
+}));
+
 app.use(compression());
 app.use(cookieParser());
-app.use(bodyParser());
+app.use(bodyParser.json());
 
 const server = http.createServer(app);
-server.listen(process.env.PORT, () => {
-  console.log(`server run on port ${process.env.PORT}`);
+const port = process.env.PORT as string;
+
+server.listen(port,()=>{
+    console.log(`active on http://localhost:${port}/`)
 });
 
+const MONGO_URL = process.env.MONGO_URL as string;
 
-const MONGO_URL = process.env.ATLAS_HOST
+mongoose.Promise = global.Promise;
+mongoose.connect(MONGO_URL);
+mongoose.connection.on('error',(error:Error)=>{
+    console.log(error)
+})
+
+app.use('/',router());
